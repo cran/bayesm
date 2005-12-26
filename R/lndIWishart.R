165f2c44acd2477dc2269e076f4849ef077d7b82
Peter Rossi <peter.rossi@chicagosb.edu>
2005-12-26 00:00:00 +0000

    version 2.0-5

--- R/lndIWishart.R
+++ R/lndIWishart.R
@@ -1,5 +1,5 @@
 lndIWishart=
-function(nu,S,IW)
+function(nu,V,IW)
 {
 # 
 # P. Rossi 12/04
@@ -9,17 +9,17 @@ function(nu,S,IW)
 #
 # arguments:
 #   nu is d. f. parm
-#   S is location matrix
+#   V is location matrix
 #   IW is the value at which the density should be evaluated
 #
 # output:
 #   value of log density
 #
-# note: in this parameterization, E[IW]=S/(nu-k-1)
+# note: in this parameterization, E[IW]=V/(nu-k-1)
 #
-k=ncol(S)
+k=ncol(V)
 Uiw=chol(IW)
-lndetSd2=sum(log(diag(chol(S))))
+lndetVd2=sum(log(diag(chol(V))))
 lndetIWd2=sum(log(diag(Uiw)))
 #
 # first evaluate constant
@@ -27,5 +27,5 @@ lndetIWd2=sum(log(diag(Uiw)))
 const=((nu*k)/2)*log(2)+((k*(k-1))/4)*log(pi)
 arg=(nu+1-c(1:k))/2
 const=const+sum(lgamma(arg))
-return(-const+nu*lndetSd2-(nu+k+1)*lndetIWd2-.5*sum(diag(S%*%chol2inv(Uiw))))
+return(-const+nu*lndetVd2-(nu+k+1)*lndetIWd2-.5*sum(diag(V%*%chol2inv(Uiw))))
 }
--- R/lndMvn.R
+++ R/lndMvn.R
@@ -2,12 +2,15 @@ lndMvn=
 function(x,mu,rooti)
 {
 #
+# changed 12/05 by Rossi to include normalizing constant
+#
 # function to evaluate log of MV NOrmal density with  mean mu, var Sigma
-# and with sigma^-1=rooti%*%t(rooti)   
+# Sigma=t(root)%*%root   (root is upper tri cholesky root)
+# Sigma^-1=rooti%*%t(rooti)   
 # rooti is in the inverse of upper triangular chol root of sigma
 #          note: this is the UL decomp of sigmai not LU!
 #                Sigma=root'root   root=inv(rooti)
 #
 z=as.vector(t(rooti)%*%(x-mu))
-return(-.5*(z%*%z) + sum(log(diag(rooti))))
+return(  -(length(x)/2)*log(2*pi) -.5*(z%*%z) + sum(log(diag(rooti))))
 }
